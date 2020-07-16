287a8f44ed6cdc49db5136bb96ee61112cda07b3
lusitong <si-lu@sunseer.co.jp>
2020-07-16 20:02:19 +0900

    ..

--- src/components/Welcome.vue
+++ src/components/Welcome.vue
@@ -12,6 +12,58 @@
       <!-- 为ECharts准备一个具备大小（宽高）的Dom -->
       <div id="main" style="width: 500px;height:350px;"></div>
     </el-card>
+
+<!--      表单区域-->
+      <template>
+<!--         <div class="common-table">-->
+<!--        -->
+<!--         </div>-->
+          <el-table
+                  :data="tableData"
+                  border
+                  style="width: 100%">
+              <el-table-column
+                      fixed
+                      prop="date"
+                      label="日期"
+                      width="150">
+              </el-table-column>
+              <el-table-column
+                      prop="name"
+                      label="姓名"
+                      width="120">
+              </el-table-column>
+              <el-table-column
+                      prop="province"
+                      label="省份"
+                      width="120">
+              </el-table-column>
+              <el-table-column
+                      prop="city"
+                      label="市区"
+                      width="120">
+              </el-table-column>
+              <el-table-column
+                      prop="address"
+                      label="地址"
+                      width="300">
+              </el-table-column>
+              <el-table-column
+                      prop="zip"
+                      label="邮编"
+                      width="120">
+              </el-table-column>
+              <el-table-column
+                      fixed="right"
+                      label="操作"
+                      width="100">
+                  <template slot-scope="scope">
+                      <el-button @click="handleClick(scope.row)" type="text" size="small">查看</el-button>
+                      <el-button type="text" size="small">编辑</el-button>
+                  </template>
+              </el-table-column>
+          </el-table>
+      </template>
   </div>
 </template>
 <script>
